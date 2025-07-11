fafeeada1d8073cf76b05287d3a5f98cc88c19d8
bllry <83239039+bllryy@users.noreply.github.com>
2025-07-11 14:51:25 -0400

    Update init.el

--- emacs-old/.emacs.d/init.el
+++ emacs-old/.emacs.d/init.el
@@ -1,3 +1,9 @@
+;; Disable UI elements and startup screen
+(setq inhibit-startup-screen t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
 ;; Initialize package system
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -17,18 +23,13 @@
   :config
   (load-theme 'gruber-darker t))
 
-
 ;; Pink UWU theme
 ;;(use-package pink-bliss-uwu-theme
 ;;  :config
 ;;  (load-theme 'pink-bliss-uwu t))
 
-
-
 (require 'elcord)
 (elcord-mode)
-
-
 
 ;; Display line numbers and highlight current line
 (global-display-line-numbers-mode 1)
@@ -47,7 +48,5 @@
  ;; If there is more than one, they won't work right.
  )
 
-
 ;; Increase default font size
 (set-face-attribute 'default nil :height 170)
-
